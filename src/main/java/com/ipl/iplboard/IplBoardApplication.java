c2204672ca0dde99e5235a90a87268ce94196b97
malikrajat <er.rajatmalik@gmail.com>
2021-10-22 00:32:33 +0530

    Initial commit

---
+++ src/main/java/com/ipl/iplboard/IplBoardApplication.java
@@ -0,0 +1,15 @@
+package com.ipl.iplboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplBoardApplication.class, args);
+	}
+
+}
+// TODO: https://www.youtube.com/watch?v=KnGKZc31P5k&list=PLqq-6Pq4lTTa8V613TZhGq4o8hSgkMGQ0&index=11
+// TODO: Length 10
